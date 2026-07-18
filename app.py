755bb9d256191eda241baa2cee95e09a6990d234
hinesglobal4 <hinesglobal4@gmail.com>
2026-07-18 12:13:31 -0400

    Add Flask application setup with home route

--- app.py
+++ app.py
@@ -13,3 +13,19 @@ from service.inventory_service import InventoryService
 from service.cycle_count_service import CycleCountService
 
 from service.kpi_service import KPIService
+
+
+from flask import Flask
+
+app = Flask(
+    __name__,
+    template_folder="frontend/templates",
+    static_folder="frontend/static"
+)
+
+@app.route("/")
+def home():
+    return "Mini WMS Running"
+
+if __name__ == "__main__":
+    app.run(debug=True)
